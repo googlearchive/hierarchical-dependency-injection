78f94c371936b2fd812d03dc087407b0290488bb
Patrice Chalin <pchalin@gmail.com>
2018-03-23 07:08:53 -0400

    Sync with 6b83dc0
    Synced with dart-lang/site-webdev 5-dev branch, commit 6b83dc0:
    https://github.com/dart-lang/site-webdev/tree/6b83dc011313d69c73e0ed4a338b438d9fcabe20/examples/ng/doc/hierarchical-dependency-injection

--- 5/packages/hierarchical_di/src/hero_tax_return_component.css.dart
+++
@@ -1,4 +0,0 @@
-library asset_hierarchical_di_lib_src_hero_tax_return_component.css.dart;
-
-
-const List<dynamic> styles = const ['.tax-return { border: thin dashed green; margin: 1em; padding: 1em; width: 18em; position: relative }\n#name { font-weight: bold;}\n#tid { float: right; }\ninput { font-size: 100%; padding-left: 2px; width: 6em; }\ninput.num { text-align: right; padding-left: 0; padding-right: 4px; width: 4em;}\nfieldset { border: 0 none;}\n\n.msg {\n  color: white;\n  font-size: 150%;\n  position: absolute;\n  /*opacity: 0.3;*/\n  left: 2px;\n  top: 3em;\n  width: 98%;\n  background-color: green;\n  text-align: center;\n}\n.msg.canceled {\n  color: white;\n  background-color: red;\n}\n'];
